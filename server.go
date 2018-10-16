452412fbba6c73b2f983822b7c754a7512769ad9
Ashlynn Pope <42753387+kesslersec@users.noreply.github.com>
2018-10-16 16:45:17 -0700

    maybe this will work

--- server.go
+++ server.go
@@ -2,20 +2,49 @@ package main
 
 import (
 		"net/http"
+		"html/template"
+		"fmt"
 		)
+		
+		var (
+			indexTemplate = template.Must(template.ParseFiles("index.gtpl"))
+			)
+
+		type templateParams struct {
+				Password string
+				Name     string
+				Notice   string
+		}
 
 func main() {
-        http.HandleFunc("/", handle)
-        if err := http.ListenAndServe(":8080",nil); err != nil {
-        	panic(err)
-        }
-
+	    http.HandleFunc("/", handle)
 		appengine.Main()
 }
 func handle(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
-	            http.Redirect(w, r, "/", http.StatusFound)
-	            return
-	        }
-	    w.Write([]byte("weow"))
+				http.Redirect(w, r, "/", http.StatusFound)
+				return
+		}
+
+		params := templateParams{}
+			
+		if r.Method == "GET" {
+			indexTemplate.Execute(w, params)
+			return
+		}
+
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+		params.Name = name
+		params.Password = password
+		if name == "ashlynn" {
+			if password == "password" {
+				params.Notice = fmt.Sprintf("Successfully logged in\nUsername: %s\nPassword: %s", username, password)
+			}
+		} else {
+			params.Notice = fmt.Sprintf("Ah ah ah, you didn't say the magic word!\nUsername: %s\nPassword: %s", username, password)
+		}
+
+		indexTemplate.Execute(w, params)
+
 }
